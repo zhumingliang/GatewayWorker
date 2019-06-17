23c642ff136762bdba96955edc9a7fa868a070a0
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-06-18 02:11:01 +0800

    初始化

--- Applications/YourApp/test_gateway.php
+++ Applications/YourApp/test_gateway.php
@@ -23,14 +23,14 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 // gateway 进程，这里使用Text协议，可以用telnet测试
 $gateway = new Gateway("Text://0.0.0.0:7272");
 // gateway名称，status方便查看
-$gateway->name = 'drive';
+$gateway->name = 'test';
 // gateway进程数
 $gateway->count = 4;
 // 本机ip，分布式部署时使用内网ip
 $gateway->lanIp = '127.0.0.1';
 // 内部通讯起始端口，假如$gateway->count=4，起始端口为4000
 // 则一般会使用4000 4001 4002 4003 4个端口作为内部通讯端口 
-$gateway->startPort = 2900;
+$gateway->startPort = 2800;
 // 服务注册地址
 $gateway->registerAddress = '127.0.0.1:1238';
 
