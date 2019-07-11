70e856f97fb68faca919a0d0b1709dc94fcbce75
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-07-11 15:13:46 +0800

    初始化

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -46,6 +46,8 @@ class Events
             '16273', 'cdb_outerroot', 'Libo1234', 'drive');
 
 
+        self::$redis = new Redis();
+        self::$redis->connect('127.0.0.1', 6379, 60);
     }
 
 
@@ -78,10 +80,10 @@ class Events
 
         try {
 
-
-            self::$redis = new Redis();
-            self::$redis->connect('127.0.0.1', 6379, 60);
-
+            $message = json_decode($message, true);
+            if (!key_exists('type', $message)) {
+                return;
+            }
             $u_id = Gateway::getUidByClientId($client_id);
             if (!$u_id) {
                 Gateway::sendToClient($client_id, json_encode([
@@ -90,57 +92,13 @@ class Events
                 ]));
                 return;
             }
-
-            $message = json_decode($message, true);
-            if (!key_exists('type', $message)) {
-                $return_data = [
-                    'errorCode' => 2,
-                    'msg' => '非法请求'
-                ];
-                Gateway::sendToClient($client_id, json_encode($return_data));
-                return;
-            }
             $type = $message['type'];
             if ($type == 'location' && key_exists('locations', $message)) {
                 $locations = $message['locations'];
-                if (!count($locations)) {
-                    Gateway::sendToClient($client_id, json_encode([
-                        'errorCode' => 3,
-                        'msg' => '地理位置信息不能为空'
-                    ]));
-                    return;
-                }
-                foreach ($locations as $k => $v) {
-                    self::$db->insert('drive_location_t')->cols(
-                        array(
-                            'lat' => $v['lat'],
-                            'lng' => $v['lng'],
-                            'phone_code' => $v['phone_code'],
-                            'create_time' => $v['create_time'],
-                            'update_time' => $v['create_time'],
-                            'o_id' => key_exists('o_id', $v) ? $v['o_id'] : '',
-                            'u_id' => $u_id
-                        )
-                    )->query();
-                  //  if ($k == 0) {
-                        //将地理位置存储到redis
-                        //1.先删除旧的实时地理位置
-                        self::$redis->rawCommand('zrem', 'drivers_tongling', $k+1);
-                        //2.新增新的实时地理位子
-                        $ret = self::$redis->rawCommand('geoadd', 'drivers_tongling', $v['lng'], $v['lat'], $k+1);
-                        if (!$ret) {
-                            Gateway::sendToClient($client_id, json_encode([
-                                'errorCode' => 7,
-                                'msg' => '写入redis失败'
-                            ]));
-                            return;
-                        }
-                   // }
-
-                }
-
-
+                self::prefixLocation($client_id, $u_id, $locations);
             }
+
+
         } catch (Exception $e) {
             Gateway::sendToClient($client_id, json_encode([
                 'errorCode' => 3,
@@ -149,6 +107,98 @@ class Events
         }
 
 
+    }
+
+    private static function prefixLocation($client_id, $u_id, $locations)
+    {
+
+
+        if (!count($locations)) {
+            Gateway::sendToClient($client_id, json_encode([
+                'errorCode' => 3,
+                'msg' => '地理位置信息不能为空'
+            ]));
+            return;
+        }
+        $old_lng = 0;
+        $old_lat = 0;
+        $old_location = self::$redis->rawCommand('geopos', 'drivers_tongling', $u_id);
+        if ($old_location) {
+            $old_lng = $old_location[0][0];
+            $old_lat = $old_location[0][1];
+        }
+
+        foreach ($locations as $k => $v) {
+            self::$db->insert('drive_location_t')->cols(
+                array(
+                    'lat' => $v['lat'],
+                    'lng' => $v['lng'],
+                    'phone_code' => $v['phone_code'],
+                    'create_time' => $v['create_time'],
+                    'update_time' => $v['create_time'],
+                    'o_id' => key_exists('o_id', $v) ? $v['o_id'] : '',
+                    'begin' => key_exists('begin', $v) ? $v['begin'] : 2,
+                    'u_id' => $u_id
+                )
+            )->query();
+            if ($k == 0) {
+                //将地理位置存储到redis,并更新行动距离
+                //1.先删除旧的实时地理位置
+                self::$redis->rawCommand('zrem', 'drivers_tongling', $u_id);
+                //2.新增新的实时地理位子
+                $ret = self::$redis->rawCommand('geoadd', 'drivers_tongling', $v['lng'], $v['lat'], $u_id);
+                if (!$ret) {
+                    Gateway::sendToClient($client_id, json_encode([
+                        'errorCode' => 7,
+                        'msg' => '写入redis失败'
+                    ]));
+                    //return;
+                }
+            }
+
+            if (key_exists('o_id', $v) && strlen($v['o_id'])
+                && key_exists('begin', $v) && $v['begin' == 1]) {
+                self::prefixDistance($v['o_id'], $old_lng, $old_lat, $v['lng'], $v['lat']);
+            }
+
+            $old_lng = $v['lng'];
+            $old_lat = $v['lng'];
+
+        }
+
+
+    }
+
+    private static function prefixDistance($o_id, $old_lng, $old_lat, $new_lng, $new_lat)
+    {
+        $order_id = 'o:' . $o_id;
+        //获取距离并增加新距离
+        $distance = self::$redis->zScore('order:distance', $order_id);
+        if ($distance === false) {
+            $res = self::$redis->zAdd('order:distance', 0, $order_id);
+            return $res;
+        }
+        if ($old_lng == 0 || $old_lat == 0 || $new_lng == 0 || $new_lat == 0) {
+
+            $dis = 0;
+        } else {
+            $dis = self::GetDistance($old_lng, $old_lat, $new_lng, $new_lat);
+        }
+        $res = self::$redis->zIncrBy('order:distance', $dis, $order_id);
+        return $res;
+    }
+
+    public static function GetDistance($lat1, $lng1, $lat2, $lng2)
+    {
+        $radLat1 = $lat1 * pi() / 180.0;
+        $radLat2 = $lat2 * pi() / 180.0;
+        $a = $radLat1 - $radLat2;
+        $b = $lng1 * pi() / 180.0 - $lng2 * pi() / 180.0;
+        $s = 2 * asin(sqrt(pow(sin($a / 2), 2) +
+                cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2)));
+        $s = $s * 6378.137;
+        $s = round($s * 10000) / 10000;
+        return $s;
     }
 
     /**
