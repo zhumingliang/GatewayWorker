fbde4ae848de675c26414c79b5bc978730f7f6d2
zhumingliang <353575156@qq.com>
2020-05-30 11:20:20 +0800

    开通端口

--- Applications/YourApp/start_driver_two_gateway.php
+++ Applications/YourApp/start_driver_two_gateway.php
@@ -23,7 +23,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 // gateway 进程，这里使用Text协议，可以用telnet测试
 $gateway = new Gateway("Websocket://0.0.0.0:7273");
 // gateway名称，status方便查看
-$gateway->name = 'Websocket_driver_one';
+$gateway->name = 'Websocket_driver_two';
 // gateway进程数
 $gateway->count = 4;
 // 本机ip，分布式部署时使用内网ip
